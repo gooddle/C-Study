cef71b8c9d033fa3c2e7a7d2a995ba9ca26a429f
mac <vptl7211@naver.com>
2026-04-07 20:38:17 +0900

    [Feat]: 기본 자료형 공부

--- 01_Variable/var_study.c
+++ 01_Variable/var_study.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 #include "var_study.h"
 
-void run_chap_1(void) {
-    printf("1단원: 변수 공부 시작!\n");
+int run_chap_1(void) {
+    //4바이트
+    int result = 10;
+    printf("%d\n", result);
+    result = result + 10;
+    //8바이트
+    constexpr double pi1 = 3.14159265358979323846;
+    //4바이트
+    constexpr float pi2 = 3.4444444f;
+    //1바이트(constexpr: 불변)
+    constexpr char alphabet = 'Z';
+    // 문자열은 char 배열
+    char name[] = "good";
+    printf("%d\n", result);
+    printf("%c\n", alphabet);
+    // float일 때 (4바이트 실수)
+    printf("%f\n", pi1);
+    // double일 때 (8바이트 실수)
+    printf("%.3lf\n", pi2);
+    // 소수점 자릿수를 제한하고 싶다면 (예: 소수점 2자리)
+    printf("%.2f\n", pi1);
+    //배열 출력
+    printf("%s\n", name);
+    return 1;
 }
--- 01_Variable/var_study.h
+++ 01_Variable/var_study.h
@@ -1,6 +1,6 @@
 #ifndef VAR_STUDY_H
 #define VAR_STUDY_H
 
-void run_chap_1(void);
+int run_chap_1(void);
 
 #endif
